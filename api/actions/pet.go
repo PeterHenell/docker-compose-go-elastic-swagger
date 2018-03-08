caec3f56924305d2c52a8186b8d74ad5a98a4440
U-CORP\petehene <petehene@T2005888.corp.tele2.com>
2018-03-08 10:53:03 +0100

    adjusting ignores

---
+++ api/actions/pet.go
@@ -0,0 +1,38 @@
+package actions
+
+import (
+	"github.com/PeterHenell/docker-compose-go-elastic-swagger/api/generated/restapi/operations/pet"
+	"github.com/go-openapi/runtime/middleware"
+)
+
+var AddPetHandler pet.AddPetHandlerFunc = func(params pet.AddPetParams, principal interface{}) middleware.Responder {
+	return middleware.NotImplemented("operation pet.AddPet has not yet been implemented")
+}
+
+var DeletePetHandler pet.DeletePetHandlerFunc = func(params pet.DeletePetParams, principal interface{}) middleware.Responder {
+	return middleware.NotImplemented("operation pet.DeletePet has not yet been implemented")
+}
+
+var FindPetsByStatusHandler pet.FindPetsByStatusHandlerFunc = func(params pet.FindPetsByStatusParams, principal interface{}) middleware.Responder {
+	return middleware.NotImplemented("operation pet.FindPetsByStatus has not yet been implemented")
+}
+
+var FindPetsByTagsHandler pet.FindPetsByTagsHandlerFunc = func(params pet.FindPetsByTagsParams, principal interface{}) middleware.Responder {
+	return middleware.NotImplemented("operation pet.FindPetsByTags has not yet been implemented")
+}
+
+var GetPetByIDHandler pet.GetPetByIDHandlerFunc = func(params pet.GetPetByIDParams, principal interface{}) middleware.Responder {
+	return middleware.NotImplemented("operation pet.GetPetByID has not yet been implemented")
+}
+
+var UpdatePetHandler pet.UpdatePetHandlerFunc = func(params pet.UpdatePetParams, principal interface{}) middleware.Responder {
+	return middleware.NotImplemented("operation pet.UpdatePet has not yet been implemented")
+}
+
+var UpdatePetWithFormHandler pet.UpdatePetWithFormHandlerFunc = func(params pet.UpdatePetWithFormParams, principal interface{}) middleware.Responder {
+	return middleware.NotImplemented("operation pet.UpdatePetWithForm has not yet been implemented")
+}
+
+var UploadFileHandler pet.UploadFileHandlerFunc = func(params pet.UploadFileParams, principal interface{}) middleware.Responder {
+	return middleware.NotImplemented("operation pet.UploadFile has not yet been implemented")
+}
